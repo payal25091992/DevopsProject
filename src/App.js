736d26f19cb8788105f1dafb5933e647754626b2
payal25091992 <payalpatil610@gmail.com>
2026-06-13 15:54:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import { useEffect, useState } from 'react';
 function App() {
   const [message, setMessage] = useState('');
 
-  useEffect(() => {
-    fetch('http://localhost:5000/api/message')
-      .then(res => res.json())
-      .then(data => setMessage(data.message))
-      .catch(err => console.log(err));
-  }, []);
+useEffect(() => {
+  fetch('http://a7443eb3efb79434284e6e8a0682b0af-864709235.ap-south-1.elb.amazonaws.com/api/message')
+    .then(res => res.json())
+    .then(data => setMessage(data.message))
+    .catch(err => console.log(err));
+}, []);
 
   return (
     <div style={{ padding: '40px', fontFamily: 'Arial' }}>
